be5a58421c2c94f803f6938b6be80ab66631d79b
Kaytise <kaytiseprof@gmail.com>
2025-06-27 03:15:18 +0600

    noting

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
+    const PI: f64 = 3.14;
+
+
 fn main() {
+    const MAX_POINTS: f64 = 100.0;
+    let is_active: bool = true;
+    let grade: char = 'A';
+    let emoji: char = '🦀';
+    
+    // Printing variables
+
+    println!("Emoji: {}", emoji);
+
     println!("Hello from rust");
     println!("My name is estiyak");
+
+    let mut x= 5;
+    println!("the value of x: {}",x );
+
+    x = 6;   
+    println!("the value of x: {}",x );
+    let counter: i32 = 0;
+    
+
+    println!("the value of pi: {}", PI); 
+    println!("Counter: {}, Max: {}, Active: {}, Grade: {}", counter, MAX_POINTS, is_active, grade);
+
 
 }
